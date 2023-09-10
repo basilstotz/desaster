849856af27a1303766ed2e7f5f0c147e6395b0f5
Basil Stotz <stotz.basil@opinsys.fi>
2023-09-10 11:14:11 +0200

    add .gitignore

--- slide/bin/build.sh
+++ slide/bin/build.sh
@@ -6,11 +6,16 @@ names="apfel birne citrone dattel emmer feige gerste hirse idared kirsche limone
 test -d $prefix && rm -r $prefix
 mkdir -p $prefix
 
+# dummy placeholder index.html
 cat <<EOF > $prefix/index.html
 <!DOCTYPE html>
 <html>
 <head>
     <title>Desastre</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width" />
 </head>
 <body>
 </body>
@@ -26,6 +31,10 @@ cat <<EOF > $prefix/dashboard.html
 <html>
 <head>
     <title>Desastre</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width" />
 </head>
 <body>
 <h1>Desaster Dashboard</h1>
@@ -41,13 +50,21 @@ for n in $names; do
 <html>
 <head>
     <title>Desastre</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width" />
 </head>
 
 <body>
-  <h1>$n</h1>
+  <h1>Desaster $n</h1>
+  <p>Scanne diesen QR-Code mit den Handy, um diese Seite mit dem Handy zu öffnen:<p>
   <img src="qrcode.png"><br/>
-  <a href="capture.html">Capture</a><br/>
-  <a href="view.html">View</a><br/>
+  <p>(Alternatif kannst du diese URL auf dem Handy auch händisch eingeben)</p> 
+  <h3>Aufnehmen</h3>
+  <p>Öffne diesen Link um die Aufnahme zu starten: <a href="capture.html">Capture</a>.</p><p>(Achtung: Du solltest dies nur einmal aktiv haben, sonst gibt ein (harmloses) Durcheinander.<p/>
+  <h3>Ansehen</h3>
+  <p>Mit diesem Link kannst du deine Aufnahme ansehen: <a href="view.html">View</a>.</p><p>(Du kannst diesen Link so oft wie du willst öffnen.)</p>
 </body>
 </html>
 EOF
@@ -75,5 +92,3 @@ setInterval(update, 5000);
 </html>
 EOF
 
-        
-        
